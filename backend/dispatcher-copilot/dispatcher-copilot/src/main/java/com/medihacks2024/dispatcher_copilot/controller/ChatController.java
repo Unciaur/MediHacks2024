fb63bcc0df55540d100d56db431b6738d309569f
Unciaur <52894100+Unciaur@users.noreply.github.com>
2024-07-08 13:34:16 -0400

    fix transcript

--- backend/dispatcher-copilot/dispatcher-copilot/src/main/java/com/medihacks2024/dispatcher_copilot/controller/ChatController.java
+++ backend/dispatcher-copilot/dispatcher-copilot/src/main/java/com/medihacks2024/dispatcher_copilot/controller/ChatController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,7 @@ public class ChatController {
     @Value("${openai.api.url}")
     private String apiUrl;
 
+    @CrossOrigin(origins = "https://medi.letssign.xyz")
     @GetMapping("/chat")
     public DeferredResult<ResponseEntity<?>> chat(@RequestParam String prompt) {
         DeferredResult<ResponseEntity<?>> output = new DeferredResult<>();
