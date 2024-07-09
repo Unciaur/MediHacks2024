33eaacbfe0565d521b9bceee178b5ddf100425d4
Unciaur <52894100+Unciaur@users.noreply.github.com>
2024-07-09 12:00:50 -0400

    revert prompt

--- backend/dispatcher-copilot/dispatcher-copilot/src/main/java/com/medihacks2024/dispatcher_copilot/controller/ChatController.java
+++ backend/dispatcher-copilot/dispatcher-copilot/src/main/java/com/medihacks2024/dispatcher_copilot/controller/ChatController.java
@@ -54,7 +54,7 @@ public class ChatController {
         ChatRequest request = new ChatRequest();
         request.setModel("Qwen/Qwen2-7B-Instruct");
         List<Message> messages = new ArrayList<>();
-        messages.add(new Message("system", "Extrapolate the name, address (or location if uncertain), and reason for call from the user's prompt. Use the name, address, and reason that has the highest repetition rate."));
+        messages.add(new Message("system", "Extrapolate the name, address (or location if uncertain), and reason for call from the user's prompt."));
         messages.add(new Message("user", prompt));
         request.setMessages(messages);
         request.setMinTokens(5);
