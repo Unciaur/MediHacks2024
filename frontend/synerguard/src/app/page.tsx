6837800ef007c2fdcac7b1c5d776f1c4cdc0a6c5
Unciaur <52894100+Unciaur@users.noreply.github.com>
2024-07-10 16:18:38 -0400

    add stuff gtg

--- frontend/synerguard/src/app/page.tsx
+++ frontend/synerguard/src/app/page.tsx
@@ -2,16 +2,26 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
+  const navbarHeight = "70px";
+
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-center p-24" style={{ backgroundColor: "rgb(238, 240, 241)" }}>
-        <div className="flex w-full justify-center">
-          <div className="bg-white rounded-lg p-4 w-1/2">
+      <main className="flex w-full flex-col" style={{ backgroundColor: "rgb(238, 240, 241)", minHeight: `calc(100vh - ${navbarHeight})`, padding: "24px", overflow: "hidden" }}>
+        <div className="flex w-full flex-1 justify-center gap-x-4">
+          <div className="bg-white rounded-lg p-4 w-1/2 flex-1 border border-gray-300">
             boo
           </div>
-          <div className="bg-white rounded-lg p-4 w-1/2">
-            test
+          {/* Larger box containing the two vertically split boxes */}
+          <div className="bg-white rounded-lg p-4 w-1/2 flex-1 border border-gray-300">
+            <div className="border border-gray-300 flex flex-col">
+              <div className="bg-white rounded-lg p-4 flex-1 border border-gray-300">
+                Top half
+              </div>
+              <div className="bg-white rounded-lg p-4 flex-1 border border-gray-300">
+                Bottom half
+              </div>
+            </div>
           </div>
         </div>
       </main>
