4e580e72cce3f1c3771ce3fdcadaf77d8f147549
Sarah Holmes <sarah.holmes916@yahoo.com>
2024-07-14 18:05:59 -0500

    improved animations

--- frontend/synerguard/src/app/page.tsx
+++ frontend/synerguard/src/app/page.tsx
@@ -45,17 +45,17 @@ const GetStartedPage = () => {
             
             <div className={"flex items-center justify-center bg-white w-screen h-[500px]"} style={{backgroundColor: 'white' }}>
             <section className="flex flex-row place-items-center justify-center items-center w-screen h-[500px]">
-                <div className={"flex flex-col items-center text-center justify-center h-[350px] w-[350px] m-auto text-3xl border rounded-xl border-gray-400"} style={{color: "rgb(50,51,56)"}}>
+                <div id="privacy" className={"flex flex-col items-center text-center justify-center h-[350px] w-[350px] m-auto text-3xl border rounded-xl border-gray-400"} style={{color: "rgb(50,51,56)"}}>
                     <Image src={lock} alt="lock" className="h-[200px] w-[200px]"/>
                     Privacy
                     <p className="text-sm mt-[20px] w-[320px]">Call transcripts and AI generated information are not sent to any other application or saved to our server to ensure user privacy.</p>
                 </div>
-                <div className={"flex flex-col items-center text-center justify-center h-[350px] w-[350px] m-auto text-3xl border rounded-xl border-gray-400"} style={{color: "rgb(50,51,56)" }}>
+                <div id="efficiency" className={"flex flex-col items-center text-center justify-center h-[350px] w-[350px] m-auto text-3xl border rounded-xl border-gray-400"} style={{color: "rgb(50,51,56)" }}>
                     <Image src={efficiency} alt="efficiency" className="h-[170px] w-[170px] m-[7px] mb-[19px]"/>
                     Efficiency
                     <p className="text-sm mt-[20px] w-[320px]">Quickly extrapolates relevant information to be sent to emergency services and improve response times. </p>
                 </div>
-                <div className={"flex flex-col items-center text-center justify-center h-[350px] w-[350px] m-auto text-3xl border rounded-xl border-gray-400"} style={{color: "rgb(50,51,56)" }}>
+                <div id="cost" className={"flex flex-col items-center text-center justify-center h-[350px] w-[350px] m-auto text-3xl border rounded-xl border-gray-400"} style={{color: "rgb(50,51,56)" }}>
                 <Image src={money} alt="money" className="h-[170px] w-[180px] m-[9px] mb-[9px]"/>
                     Cost
                     <p className="text-sm mt-[20px] w-[320px]">Completely FREE to give all the same access to life-saving technology, especially in lower-income areas affected by outdated equipment.
@@ -69,13 +69,26 @@ const GetStartedPage = () => {
         body{
         overflowY: scroll;
         }
-        section{
-        animation: fade-in linear;
-        animation-timeline: view();
-        animation-range: entry 0%;
+        #privacy {
+            scale: .8; opacity: 0;
+            animation: fade-in linear forwards;
+            animation-timeline: view();
+            animation-range: entry 0%;
+        }
+        #efficiency {
+            scale: .8; opacity: 0;
+            animation: fade-in linear forwards;
+            animation-timeline: view();
+            animation-range: entry 50%;
+            
+        }
+        #cost {
+            scale: .8; opacity: 0;
+            animation: fade-in linear forwards;
+            animation-timeline: view();
+            animation-range: entry 80%;
         }
         @keyframes fade-in{
-        from {scale: .8; opacity: 0;}
         to {scale: 1; opacity: 1;}
         }
       `}</style>
